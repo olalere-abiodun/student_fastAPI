5d7b9c7317f684efdf1450e59ff634e06b948227
Olalere Sherifdeen Abiodun <shervie20@gmail.com>
2024-02-18 17:52:43 +0100

    Changes removing HTTPexception

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -8,7 +8,7 @@ studentID = 0
 
 @app.get("/")
 def home():
-    return {"Welcome To Student FastAPI Application"}
+    return {"Welcome to My Student FastAPI application"}
 
 # creating a student
 @app.post("/student")
@@ -33,16 +33,18 @@ def student_create(name: str,age: int,sex: str,height: float):
 def student():
     return students
     
+
 # Get A Single Student information
 @app.get("/student/{student_id}")
 def student_get(student_id: int):
     student = students.get(student_id)
     if student_id in students:
         return student
-    raise HTTPException(status_code=400, detail="Student not found")
+    return ("Student not found")
 
 # Updating Student record
 @app.put("/student/{student_Id}")
+
 def student_update(student_Id: int,name: str,age: int,sex: str,height: float):
     student_data = {"Id" : 0,"Name" : "", "Age" : "", "Sex":"", "height": 0.0}
     student = students.get(student_Id)
@@ -54,9 +56,9 @@ def student_update(student_Id: int,name: str,age: int,sex: str,height: float):
         student_data["height"] = height
         students[student_Id] = student_data
         
-        return Response(message="Student updated successfully", data=student_data)
-        
-    raise HTTPException(status_code=400, detail="Student not found")
+        return student_data
+    
+    return ("Student not found")
 
 # Delete a student
 @app.delete("/student/{student_Id}")
@@ -65,7 +67,7 @@ def delete_student(student_Id:int):
     if student_Id in students:
         del students[student_Id]
         return ("Student Deleted Successfully")
-    raise HTTPException(status_code=400, detail="Student not found")
+    return ("Student not found")
 
 
 
